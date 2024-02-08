0ec8d8775e4ab62bf046f685b21be02469246eb7
Angelo Antunes <angelomw@hotmail.com>
2024-02-08 01:11:38 -0300

    criando a seção de projetos e o componente de projeto

--- src/containers/Sobre/styles.ts
+++ src/containers/Sobre/styles.ts
@@ -1,10 +1,10 @@
 import styled from 'styled-components'
 
 export const GithubSecao = styled.div`
-  margin: 32px 0 64px;
+  margin: 32px 0px 64px 0px;
 
   img {
-    width: 100%
+    width: 100%;
     height: 157px;
   }
 `
